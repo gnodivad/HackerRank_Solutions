223c1d352627b3fa7c4ad2f0cfef885e4536dc99
DavidODW <dodwei.dev@gmail.com>
2019-10-12 10:30:32 +0800

    [Regex]Update Matching Ending Items solution

--- Regex/Repetitions/matching-ending-items.java
+++ Regex/Repetitions/matching-ending-items.java
@@ -9,7 +9,7 @@ public class Solution {
     public static void main(String[] args) {
 
         Regex_Test tester = new Regex_Test();
-        tester.checker("[a-zA-Z]s$"); // Use \\ instead of using \
+        tester.checker("^[a-zA-Z]*s$"); // Use \\ instead of using \
 
     }
 }
