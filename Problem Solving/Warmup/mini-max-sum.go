f85ca711246310d07276b5a6b476e988ef9e32c6
DavidODW <dodwei.dev@gmail.com>
2020-01-12 13:38:04 +0800

    [Problem Solving]Add Mini-Max Sum solution

---
+++ Problem Solving/Warmup/mini-max-sum.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+// Complete the miniMaxSum function below.
+func miniMaxSum(arr []int64) {
+    var sum int64
+    minValue := arr[0]
+    maxValue := arr[0]
+    for _, value := range arr {
+        sum += value
+        minValue = min(minValue, value)
+        maxValue = max(maxValue, value)
+    }
+
+    fmt.Print(sum - maxValue, sum - minValue)
+}
+
+func min(x, y int64) int64 {
+    if x < y {
+        return x
+    }
+    return y
+}
+
+func max(x, y int64) int64 {
+    if x > y {
+        return x
+    }
+    return y
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 1024 * 1024)
+
+    arrTemp := strings.Split(readLine(reader), " ")
+
+    var arr []int64
+
+    for i := 0; i < 5; i++ {
+        arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+        checkError(err)
+        arrItem := int64(arrItemTemp)
+        arr = append(arr, arrItem)
+    }
+
+    miniMaxSum(arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
--- readme.md
+++ readme.md
@@ -45,6 +45,7 @@
 |  Warmup   |   [Diagonal Difference](https://www.hackerrank.com/challenges/diagonal-difference)   |    Easy    |   10   |  [<img height=15 src="logo/golang.svg">](Problem%20Solving/Warmup/diagonal-difference.go)  |
 |  Warmup   |            [Plus Minus](https://www.hackerrank.com/challenges/plus-minus)            |    Easy    |   10   |      [<img height=15 src="logo/golang.svg">](Problem%20Solving/Warmup/plus-minus.go)       |
 |  Warmup   |             [Staircase](https://www.hackerrank.com/challenges/staircase)             |    Easy    |   10   |       [<img height=15 src="logo/golang.svg">](Problem%20Solving/Warmup/staircase.go)       |
+|  Warmup   |          [Mini-Max Sum](https://www.hackerrank.com/challenges/mini-max-sum)          |    Easy    |   10   |     [<img height=15 src="logo/golang.svg">](Problem%20Solving/Warmup/mini-max-sum.go)      |
 |  Warmup   | [Birthday Cake Candles](https://www.hackerrank.com/challenges/birthday-cake-candles) |    Easy    |   10   | [<img height=15 src="logo/java.svg">](Problem%20Solving/Warmup/birthday-cake-candles.java) |
 
 # Java
